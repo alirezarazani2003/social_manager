fc28bda5ba7fa3a0dc7b51891d2206d0ae09bd7f
alireza <alirezarazani3185@gmail.com>
2025-12-15 11:10:46 +0330

    add favicon

--- Front-end/src/components/Dashboard/PostEditor.js
+++ Front-end/src/components/Dashboard/PostEditor.js
@@ -5,7 +5,8 @@ import DatePicker from "react-multi-date-picker";
 import TimePicker from "react-multi-date-picker/plugins/time_picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
-
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const PostEditor = () => {
   const [channels, setChannels] = useState([]);
